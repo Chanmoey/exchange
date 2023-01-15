1d3bfe1637e10e80193dc09265fa59649c08c1b2
Chanmoey <relijifa@gmail.com>
2023-01-16 05:41:32 +0800

    创建项目

---
+++ counter/src/test/java/com/moon/exchange/counter/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.exchange.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
